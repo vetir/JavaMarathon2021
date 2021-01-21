c2015e8f563fade3e6e7b49b173bf8352b316f72
vetir@mail.ru <vetir@mail.ru>
2021-01-21 11:05:09 +0300

    Solution Day 2

--- src/main/java/day2/Task1.java
+++ src/main/java/day2/Task1.java
@@ -1,7 +1,39 @@
 package day2;
 
-public class Task1 {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Task1 {
+//    1. Реализовать программу, которая принимает на вход через консоль с помощью класса Scanner,
+//    число, соответствующее количеству этажей в здании. Используя условный оператор if, необходимо
+//    вывести в консоль сообщение о типе такого дома.
+//    Условия:
+//    если этажей 1-4 - “Малоэтажный дом”, 5-8 - “Среднеэтажный дом”, 9 и более - “Многоэтажный дом”.
+//    Также, необходимо учесть что может быть введено отрицательное значение, в таком случае сообщить “Ошибка ввода”.
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        if (scanner.hasNextInt()) {
+
+            int number = scanner.nextInt();
+
+            if (number >= 1 && number <= 4) {
+                System.out.println("Малоэтажный дом");
+            }
+
+            if (number >= 5 && number <= 8) {
+                System.out.println("Среднеэтажный дом");
+            }
+
+            if (number >= 9) {
+                System.out.println("Многоэтажный дом");
+            }
+
+            if (number <=0) {
+                System.out.println("Ошибка ввода");
+            }
+
+        } else {
+            System.out.println("Ошибка ввода");
+        }
     }
 }
--- src/main/java/day2/Task3.java
+++ src/main/java/day2/Task3.java
@@ -1,7 +1,29 @@
 package day2;
 
-public class Task3 {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Task3 {
+//    3. Реализовать программу №2, используя цикл while.
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        if (scanner.hasNextInt()) {
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+
+            if (b - a <= 0) {
+                System.out.println("Некорректный ввод");
+            } else {
+                System.out.println("Вывод: ");
+                while (a < b) {
+                    a++;
+                    if (a % 5 == 0 && a % 10 != 0) {
+                        System.out.print(a + " ");
+                    }
+                }
+            }
+        } else {
+            System.out.println("Некорректный ввод");
+        }
     }
 }
