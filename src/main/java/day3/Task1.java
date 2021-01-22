d0cf416e39b54f2e3ef378942c46ac62b23645fe
vetir@mail.ru <vetir@mail.ru>
2021-01-22 10:11:26 +0300

    Solution Day 3

--- src/main/java/day3/Task1.java
+++ src/main/java/day3/Task1.java
@@ -1,7 +1,57 @@
 package day3;
 
-public class Task1 {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Task1 {
+//    1. Реализовать программу, которая в консоль выводит название страны, принимая на вход название города.
+//    Программа должна работать до тех пор, пока не будет введено слово “Stop”.
+//    Реализовать, используя следующие данные:
+//    Москва, Владивосток, Ростов - Россия
+//    Рим, Милан, Турин - Италия
+//    Ливерпуль, Манчестер, Лондон - Англия
+//    Берлин, Мюнхен, Кёльн - Германия
+//
+//    При вводе любого другого города, вывести сообщение “Неизвестная страна”.
+//
+//    Пример:
+//    Милан - ваш ввод в консоль
+//    Италия - ответ программы
+//    Дублин - ваш ввод в консоль
+//    Неизвестная страна - ответ программы
+//    Stop - ваш ввод в консоль
+//    *программа завершила работу*
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (scanner.hasNext()) {
+            String str1 = scanner.next();
+            if (str1.equals("Stop")) {
+                break;
+            }
+            switch (str1) {
+                case "Москва":
+                case "Владивосток":
+                case "Ростов":
+                    System.out.println("Россия");
+                    break;
+                case "Рим":
+                case "Милан":
+                case "Турин":
+                    System.out.println("Италия");
+                    break;
+                case "Ливерпуль":
+                case "Манчестер":
+                case "Лондон":
+                    System.out.println("Англия");
+                    break;
+                case "Берлин":
+                case "Мюнхен":
+                case "Кёльн":
+                    System.out.println("Германия");
+                    break;
+                default:
+                    System.out.println("Неизвестная страна");
+            }
+        }
     }
 }
--- src/main/java/day3/Task2.java
+++ src/main/java/day3/Task2.java
@@ -1,7 +1,33 @@
 package day3;
 
-public class Task2 {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Task2 {
+    //    2. Реализовать программу, которая пока работает, принимает на вход от пользователя два числа - делимое и делитель.
+//    Для этих двух чисел программа рассчитывает результат деления и выводит его в консоль. Программа останавливает свою
+//    работу тогда, когда пользователь вводит 0 в качестве делителя.
+//    (для этих вещественных чисел необходимо использовать тип double и метод nextDouble() у Scanner’а соответственно).
+//
+//    Пример:
+//    1 2 - ваш ввод в консоль
+//    0.5 - ответ программы
+//    100 77 - ваш ввод в консоль
+//    1.2987012987012987 - ответ программы
+//    3 0 - ваш ввод в консоль
+//    работа программы завершается
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (scanner.hasNext()) {
+
+            double num1 = scanner.nextDouble();
+            double num2 = scanner.nextDouble();
+
+            if (num2 == 0) {
+                break;
+            } else {
+                System.out.println(num1 / num2);
+            }
+        }
     }
 }
