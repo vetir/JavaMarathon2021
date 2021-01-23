0252fd27aa770f2577d9db09ccc30d95eabc361b
vetir@mail.ru <vetir@mail.ru>
2021-01-23 20:34:57 +0300

    Solution Day 4

--- src/main/java/day4/Task4.java
+++ src/main/java/day4/Task4.java
@@ -25,7 +25,7 @@ public class Task4 {
         for (int i = 0; i < array.length; i++) {
             array[i] = (int) (Math.random() * 10001);
         }
-        System.out.println(Arrays.toString(array));
+
 
         for (int i = 0; i < array.length - 2; i++) {
             sum = array[i] + array[i+1] + array[i+2];
